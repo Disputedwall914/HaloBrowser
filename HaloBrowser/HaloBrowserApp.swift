2b43b4dcd3205a1246691fea6412c9be2abe9573
Julian Bühler <julian.buehler@outlook.de>
2026-03-07 16:09:49 +0100

    Initial Commit

---
+++ HaloBrowser/HaloBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaloBrowserApp.swift
+//  HaloBrowser
+//
+//  Created by Julian on 07.03.26.
+//
+
+import SwiftUI
+
+@main
+struct HaloBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
